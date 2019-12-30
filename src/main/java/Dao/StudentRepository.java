44909cec85b2ed3901193fe8d930481bcc9c110e
vagif4799 <quliyev_vaqif@icloud.com>
2019-12-30 21:19:43 +0400

    -- Student entity added

---
+++ src/main/java/Dao/StudentRepository.java
@@ -0,0 +1,7 @@
+package Dao;
+
+import com.vagif.entity.Student;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+}
